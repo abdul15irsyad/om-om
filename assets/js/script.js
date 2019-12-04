71018923f00a37a18330328dcc1cbd083edce1ec
Irsyad Abdul <abdulirsyad15@gmail.com>
2019-12-04 12:43:10 +0700

    4des2019

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    $('#about .isi').attr("data-aos","fade-right");
+    $('#about .image-tentang').attr("data-aos","fade-left");
+  } else {
+    $('#about .isi').attr("data-aos","fade-up");
+    $('#about .image-tentang').attr("data-aos","fade-up");
+  }
+}
+
+var x = window.matchMedia("(min-width: 991px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
 $(document).ready(function(){
   
 });
